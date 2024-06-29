334c5328c81a8e6e9b7bad028ba9f39b103312a9
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-06-29 20:32:54 +0200

    len and capacity

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Print("hello")
+	var x []int
+	fmt.Println(x, len(x), cap(x))
+	x = append(x,10 )
+	fmt.Println(x, len(x), cap(x))
+	x = append(x,20 )
+	fmt.Println(x, len(x), cap(x))
+	x = append(x,30 )
+	fmt.Println(x, len(x), cap(x))
+	x = append(x,40 )
+	fmt.Println(x, len(x), cap(x))
+	x = append(x,50 )
+	fmt.Println(x, len(x), cap(x))
+
 }
